55db4fa62ceaca190e0c70a2243c352eaa73cdca
Thiago Grillo <thiago_grillo@hotmail.com>
2022-05-11 12:20:31 -0300

    #13 Yii2 - Asset Bundles (assets publicados)
    ficam na /web/ a disposicao para serem usados

---
+++ controllers/AssetsController.php
@@ -0,0 +1,13 @@
+<?php 
+
+namespace app\controllers;
+
+use yii\web\Controller;
+
+class AssetsController extends Controller{
+    public $layout = 'blank';
+
+    public function actionIndex(){
+        return $this->render('index', []);
+    }
+}
---
+++ views/assets/index.php
@@ -0,0 +1,10 @@
+<?php 
+use app\assets\meusAssets\PrimeiroAsset;
+PrimeiroAsset::register($this);
+?>
+
+<h1>Testando</h1>
+
+<p>blablabla</p>
+<p>blablabla</p>
+<p>blablabla</p>
